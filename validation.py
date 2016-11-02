c06edc43fc8cfb0e387254037368445e13f226e2
lemoer <git@irrelefant.net>
2016-11-02 21:31:55 +0100

    validation: Add basic validation skeletons
    Already possible validations:
    - type
    - less than (integer)

---
+++ validation.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+
+import unittest
+
+
+class WrongType(Exception):
+    pass
+
+
+class UnknownField(Exception):
+    pass
+
+
+class LessThan(object):
+
+    def __init__(self, other):
+        self.other = other
+
+    def validate(self, name, value):
+        if value >= self.other:
+            raise Exception(
+                'Field {} should be less than {}, but is {}'
+                .format(name, self.other, value)
+            )
+
+
+class Type(object):
+
+    def __init__(self, type):
+        self.type = type
+
+    def validate(self, name, value):
+        if type(value) is not self.type:
+            raise WrongType(
+                'Field {} should be of type {}'.format(name, self.type)
+            )
+
+
+class ValidatableObject(object):
+
+    def __init__(self, validators, **kwargs):
+        self._validators = validators
+        self._data = {}
+
+    def __setattr__(self, field_name, field_value):
+        if field_name in ['_data', '_validators']:
+            self.__dict__[field_name] = field_value
+            return
+
+        field_validators = self._validators.get(field_name, None)
+
+        if field_validators is None:
+            # TODO: add class name to message
+            raise UnknownField('Field {} is unknown'.format(field_name))
+
+        for v in field_validators:
+            v.validate(field_name, field_value)
+
+        self._data[field_name] = field_value
+
+    def __getattr__(self, field_name):
+        if field_name not in self._validators.keys():
+            # TODO: add class name to message
+            raise UnknownField('Field {} is unknown'.format(field_name))
+
+        return self._data.get(field_name, None)
+
+
+class TestValidatableObject(unittest.TestCase):
+
+    def setUp(self):
+        self.test_obj = ValidatableObject(validators={
+            'name': [Type(str)],
+            'amount': [Type(int), LessThan(5)]
+        })
+
+    def test_set_unknown_field(self):
+        with self.assertRaises(UnknownField):
+            self.test_obj.foo = 'bar'
+
+    def test_get_unknown_field(self):
+        with self.assertRaises(UnknownField):
+            print(self.test_obj.foo)
+
+    def test_set_wrong_type_int(self):
+        with self.assertRaises(WrongType):
+            self.test_obj.amount = 'banane'
+
+    def test_set_int(self):
+        self.test_obj.amount = 2
+
+    def test_set_wrong_type_string(self):
+        with self.assertRaises(WrongType):
+            self.test_obj.name = 2
+
+    def test_set_string(self):
+        self.test_obj.name = 'banane'
+
+    def test_less_than_exception(self):
+        with self.assertRaises(Exception):
+            self.test_obj.amount = 42
