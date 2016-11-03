g3n35i5/shop-db

commit d5fb739a33b097f0eb70ca9e81dd46b1785fb1b3

validation: lazy creation of ValidatableObject by using kwargs in constructor

validation.py
@@ -38,9 +38,11 @@ class Type(object):
 
 class ValidatableObject(object):
 
-    def __init__(self, validators, **kwargs):
-        self._validators = validators
+    def __init__(self, **kwargs):
         self._data = {}
+
+        for k, v in kwargs.items():
+            setattr(self, k, v)
 
     def __setattr__(self, field_name, field_value):
         if field_name in ['_data', '_validators']:

models.py
@@ -23,14 +23,13 @@ def representation(obj):
 
 class Product(ValidatableObject):
 
-    def __init__(self):
-        ValidatableObject.__init__(self, {
-            'id': [Type(int)],
-            'name': [Type(str)],
-            'price': [Type(int)],
-            'active': [Type(bool)],
-            'on_stock': [Type(bool)]
-        })
+    _validators = {
+        'id': [Type(int)],
+        'name': [Type(str)],
+        'price': [Type(int)],
+        'active': [Type(bool)],
+        'on_stock': [Type(bool)]
+    }
 
     def __repr__(self):
         return representation(self)

commit 1cacfd2d1089f5cc41bcaf1cf3f149ed7881e789

validation: fix unittest issue (introduced by d5fb739a33)

validation.py
@@ -71,10 +71,13 @@ class ValidatableObject(object):
 class TestValidatableObject(unittest.TestCase):
 
     def setUp(self):
-        self.test_obj = ValidatableObject(validators={
-            'name': [Type(str)],
-            'amount': [Type(int), LessThan(5)]
-        })
+        class TestClass(ValidatableObject):
+            _validators = {
+                'name': [Type(str)],
+                'amount': [Type(int), LessThan(5)]
+            }
+
+        self.test_obj = TestClass()
 
     def test_set_unknown_field(self):
         with self.assertRaises(UnknownField):