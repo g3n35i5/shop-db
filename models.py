eec9903941939243321caeee4b50dcae9742b7cc
g3n35i5 <gitlab@coconutpete.de>
2016-11-11 14:05:27 +0100

    models: created models for database
    Models available:
    - Consumer
    - Product
    - Purchase
    - Deposit

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from validation import ValidatableObject, Type, fields
+import datetime
 
 
 def representation(obj):
@@ -21,14 +22,51 @@ def representation(obj):
     return ''.join(r)
 
 
-class Product(ValidatableObject):
+class Consumer(ValidatableObject):
+    _validators = {
+        'id': [Type(int)],
+        'name': [Type(str)],
+        'credit': [Type(int)],
+        'active': [Type(bool)]
+    }
 
+    def __repr__(self):
+        return representation(self)
+
+
+class Product(ValidatableObject):
     _validators = {
         'id': [Type(int)],
         'name': [Type(str)],
         'price': [Type(int)],
         'active': [Type(bool)],
         'on_stock': [Type(bool)]
+    }
+
+    def __repr__(self):
+        return representation(self)
+
+
+class Purchase(ValidatableObject):
+    _validators = {
+        'id': [Type(int)],
+        'consumer_id': [Type(int)],
+        'product_id': [Type(int)],
+        'timestamp': [Type(datetime.datetime)],
+        'revoked': [Type(bool)],
+        'paid_price': [Type(int)]
+    }
+
+    def __repr__(self):
+        return representation(self)
+
+
+class Deposit(ValidatableObject):
+    _validators = {
+        'id': [Type(int)],
+        'consumer_id': [Type(int)],
+        'timestamp': [Type(datetime.datetime)],
+        'amount': [Type(int)]
     }
 
     def __repr__(self):
