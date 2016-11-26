557760e08fc9c2203ba548d0635d4e33f89d0c27
lemoer <git@irrelefant.net>
2016-11-26 18:43:10 +0100

    validation: tests are now outsourced in validation_test
    This follows the convention that every test file
    should be named as the module it tests concated
    with the suffix "_test".
    
    This commit also adds the validation tests to the global
    test.py.

---
+++ backend/validation_test.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import unittest
+from .validation import *
+
+
+class TestValidatableObject(unittest.TestCase):
+
+    def setUp(self):
+        class TestClass(ValidatableObject):
+            _validators = {
+                'name': [Type(str), MaxLength(20)],
+                'amount': [Type(int), LessThan(5)]
+            }
+
+        self.test_obj = TestClass()
+
+    def test_string_too_long(self):
+        with self.assertRaises(MaxLengthExceeded):
+            self.test_obj.name = "thisfieldismuchtoolong"
+
+    def test_set_unknown_field(self):
+        with self.assertRaises(UnknownField):
+            self.test_obj.foo = 'bar'
+
+    def test_get_unknown_field(self):
+        with self.assertRaises(UnknownField):
+            print(self.test_obj.foo)
+
+    def test_set_wrong_type_int(self):
+        with self.assertRaises(WrongType):
+            self.test_obj.amount = 'banane'
+
+    def test_set_int(self):
+        self.test_obj.amount = 2
+
+    def test_set_wrong_type_string(self):
+        with self.assertRaises(WrongType):
+            self.test_obj.name = 2
+
+    def test_set_string(self):
+        self.test_obj.name = 'banane'
+
+    def test_less_than_exception(self):
+        with self.assertRaises(Exception):
+            self.test_obj.amount = 42
--- test.py
+++ test.py
@@ -2,6 +2,7 @@
 
 import unittest
 from backend.db_api_test import *
+from backend.validation_test import *
 
 
 if __name__ == '__main__':
