a5bb6945b7817afc2bb63d44d5dbb6461212e0a4
lemoer <git@irrelefant.net>
2017-02-04 03:32:56 +0100

    Add some shell/curl scripts to create consumers and products.

---
+++ scripts/create_consumer.sh
@@ -0,0 +1,13 @@
+HOST="${HOST:-localhost:5000}"
+
+curl \
+	-X POST \
+	--header 'Content-Type: application/json' \
+	--data-binary @- \
+"http://${HOST}/consumers" <<EOF
+{
+	"name": "$1",
+	"active": true,
+	"credit": 0
+}
+EOF
