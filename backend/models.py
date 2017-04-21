80db4c9bf8cecfa613f722767b6acb0e9f632aa8
g3n35i5 <github@coconutpete.de>
2017-04-21 16:14:18 +0200

    introducing new database schema.
    - tables are now named with in plural for better readability
    
    new tables:
    - departments
    - banks
    - payoffs

--- backend/models.py
+++ backend/models.py
@@ -35,11 +35,25 @@ class Consumer(ValidatableObject):
         return representation(self)
 
 
+class Department(ValidatableObject):
+    _validators = {
+        'id': [Type(int)],
+        'name': [Type(str), MaxLength(64), MinLength(4)],
+        'income': [Type(int)],
+        'expenses': [Type(int)],
+        'budget': [Type(int)]
+    }
+
+    def __repr__(self):
+        return representation(self)
+
+
 class Product(ValidatableObject):
     _validators = {
         'id': [Type(int)],
         'name': [Type(str), MaxLength(64), MinLength(4)],
         'price': [Type(int)],
+        'department_id': [Type(int)],
         'active': [Type(bool)],
         'on_stock': [Type(bool)]
     }
@@ -53,7 +67,6 @@ class Purchase(ValidatableObject):
         'id': [Type(int)],
         'consumer_id': [Type(int)],
         'product_id': [Type(int)],
-        'comment': [Type(str), MaxLength(128), MinLength(8)],
         'amount': [Type(int)],
         'comment': [Type(str), MaxLength(64), MinLength(8)],
         'timestamp': [Type(datetime.datetime)],
@@ -69,10 +82,34 @@ class Deposit(ValidatableObject):
     _validators = {
         'id': [Type(int)],
         'consumer_id': [Type(int)],
-        'timestamp': [Type(datetime.datetime)],
-        'amount': [Type(int)]
+        'amount': [Type(int)],
         'comment': [Type(str), MaxLength(64), MinLength(8)],
+        'timestamp': [Type(datetime.datetime)]
+    }
+
+    def __repr__(self):
+        return representation(self)
+
+
+class Payoff(ValidatableObject):
+    _validators = {
+        'id': [Type(int)],
+        'bank_id': [Type(int)],
+        'department_id': [Type(int)],
         'comment': [Type(str), MaxLength(64), MinLength(8)],
+        'amount': [Type(int)],
+        'timestamp': [Type(datetime.datetime)]
+    }
+
+    def __repr__(self):
+        return representation(self)
+
+
+class Bank(ValidatableObject):
+    _validators = {
+        'id': [Type(int)],
+        'name': [Type(str), MaxLength(64), MinLength(4)],
+        'credit': [Type(int)]
     }
 
     def __repr__(self):
--- models.sql
+++ models.sql
@@ -1,4 +1,4 @@
-CREATE TABLE consumer (
+CREATE TABLE consumers (
 	id INTEGER NOT NULL,
 	name VARCHAR(64) NOT NULL,
 	active BOOLEAN NOT NULL,
@@ -8,19 +8,30 @@ CREATE TABLE consumer (
 	CHECK (active IN (0, 1))
 );
 
-CREATE TABLE product (
+CREATE TABLE departments (
+	id INTEGER NOT NULL,
+	name VARCHAR(64) NOT NULL,
+	income INTEGER NOT NULL,
+	expenses INTEGER NOT NULL,
+	budget INTEGER NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE products (
 	id INTEGER NOT NULL,
 	name VARCHAR(64) NOT NULL,
 	price INTEGER NOT NULL,
+	department_id INTEGER NOT NULL,
 	active BOOLEAN NOT NULL,
 	on_stock BOOLEAN NOT NULL,
 	PRIMARY KEY (id),
 	UNIQUE (name),
+	FOREIGN KEY(department_id) REFERENCES departments (id),
 	CHECK (active IN (0, 1)),
 	CHECK (on_stock IN (0, 1))
 );
 
-CREATE TABLE purchase (
+CREATE TABLE purchases (
 	id INTEGER NOT NULL,
 	consumer_id INTEGER NOT NULL,
 	amount INTEGER NOT NULL,
@@ -35,7 +46,7 @@ CREATE TABLE purchase (
 	CHECK (revoked IN (0, 1))
 );
 
-CREATE TABLE deposit (
+CREATE TABLE deposits (
 	id INTEGER NOT NULL,
 	consumer_id INTEGER NOT NULL,
 	amount INTEGER NOT NULL,
@@ -43,4 +54,23 @@ CREATE TABLE deposit (
 	timestamp TIMESTAMP NOT NULL,
 	PRIMARY KEY (id),
 	FOREIGN KEY(consumer_id) REFERENCES consumer (id)
+);
+
+CREATE TABLE payoffs (
+	id INTEGER NOT NULL,
+	bank_id INTEGER NOT NULL,
+	department_id INTEGER NOT NULL,
+	comment VARCHAR(64) NOT NULL,
+	amount INTEGER NOT NULL,
+	timestamp TIMESTAMP NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (bank_id) REFERENCES bank (id),
+	FOREIGN KEY (department_id) REFERENCES departments (id)
+);
+
+CREATE TABLE banks (
+	id INTEGER NOT NULL,
+	name VARCHAR(64) NOT NULL,
+	credit INTEGER NOT NULL,
+	PRIMARY KEY (id)
 );
