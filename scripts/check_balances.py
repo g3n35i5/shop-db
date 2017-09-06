f86dabec934370ef6272dd40b904d17cf151c52c
lemoer <git@irrelefant.net>
2017-09-06 15:12:49 +0200

    scripts: Add new script to check whether the initial balances of all consumers are 0

---
+++ scripts/check_balances.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+
+# This script takes all purchases and deposits and sums their
+# amount for each user. So at least, this value should be
+# equal to the actual balance of the user, if there was no
+# initial_balane for the consumer.
+#
+# initial_balance is highly undesirable, since there does not
+# exist a way (besides calculating it backwards) to see this
+# initial_balance. The only way to calculate it, is this script.
+#
+# So use this script to ensure, there was no calculation mistake!
+# (which is the case, if all initial_balance values are 0)
+
+# This script won't change anything on the running instance!
+# It just does some GET requests.
+
+import requests
+from operator import itemgetter
+
+server = 'http://shop.gatrobe.de:5000/'
+
+if not server.endswith('/'):
+    server += '/'
+
+def get_consumers():
+    return requests.get(server + 'consumers').json()
+
+def get_consumer(cid):
+    return requests.get(server + 'consumer/' + str(cid)).json()
+
+def get_purchases(cid):
+    return requests.get(server + 'consumer/' + str(cid) + '/purchases').json()
+
+def get_deposits(cid):
+    return requests.get(server + 'consumer/' + str(cid) + '/deposits').json()
+
+def calc_theoretical_amount(cid):
+    purchases = get_purchases(cid)
+    deposits = get_deposits(cid)
+
+    d_amount = sum(map(itemgetter('amount'), deposits))
+    p_amount = -sum(map(itemgetter('paid_base_price_per_product'), purchases))
+    k_amount = -sum(map(itemgetter('paid_karma_per_product'), purchases))
+
+    return sum([d_amount, p_amount, k_amount])
+
+def actual_amount(cid):
+    return get_consumer(cid)['credit']
+
+
+if __name__ == '__main__':
+    consumers = get_consumers()
+
+    print('{:25} {:>10} {:>10} {:>20}'.format('NAME', 'theoretical', 'actual', 'initial_balance'))
+    print()
+
+    for c in consumers:
+        theoretical = calc_theoretical_amount(c['id']) / 100
+        actual = actual_amount(c['id']) / 100
+
+        initial_balance = actual - theoretical
+
+        print('{:25} {:10.2f} {:10.2f} {:20.2f}'.format(c['name'], theoretical, actual, initial_balance))
+
